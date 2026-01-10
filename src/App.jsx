c7f5019e6f31c6a0780c3f76ad8b159e500623e4
raja2006-jpg <youremail@example.com>
2026-01-10 23:44:56 +0530

    Fix import paths (remove space in ../components)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Services from "./components/Services";
-import Contact from "./components/Contact";
-
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Services from "../components/Services";
+import Contact from "../components/Contact";
 
 function App() {
   return (
@@ -18,3 +17,4 @@ function App() {
 }
 
 export default App;
+  
